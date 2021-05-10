8aa7ad5daf20c36943927e6ffae072fd93e7bc5e
SalomeKelaptrishvili <83080867+SalomeKelaptrishvili@users.noreply.github.com>
2021-05-11 00:53:29 +0400

    Add files via upload

--- bog2homework.js
+++ bog2homework.js
@@ -19,43 +19,109 @@ for(let i = 0; i < tab_array.length; i++){
     });
 }
 
-
-
-
-
+//classes
 class config{
     constructor(url,contentId){
         this.url = url;
-        this.contentId=contentId;
+        this.contentId = contentId;
     }
 }
 
+
 class catalog extends config{
+    
     showGifs(){
-        fetch(this.url)
-        .then((response)=>{
-            return response.json();
-        })
-        .then((response)=>{
-            console.log(response);
-            response.forEach((item)=>{
-                const node = document.createElement('div');
-                node.innerHTML = `id: ${item.id} <br> userId: ${item.userId} <br> Title: ${item.title} <br> Body: ${item.body}`;
-                document.getElementById(this.contentId).appendChild(node);
-                //node.className = "postClass";
-            });
-        });
+        async function showGifsList(url,contentId){
+            try{
+                const response = await fetch(url,{method:'GET'});
+                const result = await response.json();
+                console.log(result);
+                let gifList = result.data;
+                for(let i in gifList){
+                    let newGif = document.createElement('img');
+                    let newLink = gifList[i].images.original.url;
+                    newGif.setAttribute("src", newLink);
+                    document.getElementById(contentId).appendChild(newGif);
+                    newGif.setAttribute("width","150px");
+                }
+            } catch(error){
+                console.log(error);
+            }
+        }
+        showGifsList(this.url,this.contentId);
+        
+        // fetch(this.url,{
+        //     method: 'GET',
+        // })
+        // .then((response)=>{
+        //     return response.json();
+        // })
+        // .then((response)=>{
+        //     console.log(response);
+        //     let gifList = response.data;
+        //     console.log(gifList);
+        //     for(let i in gifList){
+        //         let newGif = document.createElement('img');
+        //         let newLink = gifList[i].images.original.url;
+        //         newGif.setAttribute("src", newLink);
+        //         document.getElementById(this.contentId).appendChild(newGif);
+        //         newGif.setAttribute("width","150px");
+        //         console.log(newLink);
+        //     }
+        // });
     }     
 }
-const urlPosts = 'https://jsonplaceholder.typicode.com/posts?userId=1';//aq linki katebis gifebis????
-const postres = "catsResult";
 
-const url2 = 'https://jsonplaceholder.typicode.com/posts?userId=2';//aq linki memebis gifebis????
-const content2id = "memesResult";
+let searchKeyWord = "funnycat";
+let urlgif = `https://api.giphy.com/v1/gifs/search?q=${searchKeyWord}&api_key=aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB`;
 
-const tab1Catalog = new catalog(urlPosts,postres);
-tab1Catalog.showGifs();
-const tab2Catalog = new catalog(url2,content2id);
-tab2Catalog.showGifs();
+let gifStoreId = "gifresult";
+
+const gifCatalog = new catalog(urlgif,gifStoreId);
+gifCatalog.showGifs();
 
 
+
+
+
+
+
+
+
+
+
+
+//links and key
+// const queryUrl = "https://api.giphy.com/v1/gifs/search?";
+// const apiKey = "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB",
+// const trendingUrl = "https://api.giphy.com/v1/gifs/trending?";
+
+// const urlGifs = 'https://api.giphy.com/v1/gifs/search?q=Internet%20Cats&limit=10&api_key=aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB&fmt=json';//aq linki katebis gifebis????
+// const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
+// const postres = "catsResult";
+
+// const url2 = 'https://jsonplaceholder.typicode.com/posts?userId=2';//aq linki memebis gifebis????
+// const content2id = "memesResult";
+
+// const tab1Catalog = new catalog(urlGifs,postres);
+// tab1Catalog.showGifs();
+
+// const tab2Catalog = new catalog(urlGifs,content2id);
+// tab2Catalog.showGifs();
+
+// let buttonList = document.getElementsByClassName('tab-item');
+
+// let inputValue = document.getElementById("inputValue").value; //mgoni es ar mushaobs
+// console.log("inputvalue: "+ inputValue);
+
+// function addSearchButtons(){
+//     for(let i = 0; i < buttonList.length; i++){
+//         if(buttonList.length < 6){
+//             let newButton = document.createElement('button');
+//             newButton.value = inputValue;
+//             newButton.classList.add('tab-item');
+//             document.getElementById('tabList').innerHTML = newButton;
+//             buttonList.push(newButton);
+//         }
+//     }
+// }
